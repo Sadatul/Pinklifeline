4295a5322fee543f6edca3371148bc8be99025e2
hasnainadil <2005115@ugrad.cse.buet.ac.bd>
2024-07-07 14:38:42 +0600

    backing up to online. timestamp: Sun 07/07/2024 14:38:41

---
+++ frontend/src/app/(inside)/inbox/page.js
@@ -0,0 +1,9 @@
+'use client'
+import { ChatLayout } from '@/app/components/chatComponents';
+import Image from 'next/image';
+
+export default function DashBoard() {
+  return (
+      <ChatLayout />
+  );
+}
