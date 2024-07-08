90c3237fee7a1d8dc422c0ef71907e2035113e2c
Sadatul <sadatulislamsadi@gmail.com>
2024-07-08 14:53:30 +0600

    Added entities and tables

---
+++ backend/pinklifeline/src/main/java/com/sadi/pinklifeline/models/entities/DoctorConsultationLocations.java
@@ -0,0 +1,37 @@
+package com.sadi.pinklifeline.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "doctor_consultation_locations")
+public class DoctorConsultationLocations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private DoctorDetails doctorDetails;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private LocalTime start;
+
+    @Column(nullable = false)
+    private LocalTime end;
+
+    @Column(nullable = false)
+    private String workdays;
+}
