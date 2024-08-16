531afe1741f7128453ac90e0bbe6f5571f1bfd76
hasnainadil <2005115@ugrad.cse.buet.ac.bd>
2024-08-16 16:22:19 +0600

    backing up to online. timestamp: Fri 08/16/2024 16:22:16

--- frontend/src/app/(home)/(inside)/dashboard/selftest/page.js
+++ frontend/src/app/(home)/(inside)/dashboard/selftest/page.js
@@ -9,7 +9,7 @@ import { useRef, useState } from "react";
 // import selfTestAnimationMuted from "../../../../../../public/selftest/selttestanimationmuted.mp4"
 
 export default function SelfTestPage() {
-    const responses = useRef(['', '', '', '']);
+    const [responses, setResponses] = useState(['', '', '', '']);
     const [questionCompletedCount, setQuestionCompletedCount] = useState(0);
     const questionsArray = ["1. Have you noticed any changes in size or shape of your breast or nipples?", "2. Have you noticed any skin redness, rashes, dimpling, or puckering?", "3. After raising your arms, have you noticed any changes?", "4. Have you felt any lumps or painful areas?"];
 
@@ -23,21 +23,9 @@ export default function SelfTestPage() {
         };
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = () => {
         let timer;
-        const question1 = document.getElementById("question-1").value;
-        const question2 = document.getElementById("question-2").value;
-        const question3 = document.getElementById("question-3").value;
-        const question4 = document.getElementById("question-4").value;
-        if (question1 === "" || question2 === "" || question3 === "" || question4 === "") {
-            document.getElementById("errormsg").innerText = "Please answer all questions";
-            return;
-        }
-        else {
-            document.getElementById("errormsg").innerText = "";
-        }
-        if (responses.current.includes("yes")) {
+        if (responses.includes("yes")) {
             setResultState("loading");
             timer = setTimeout(() => {
                 setResultState("warn");
@@ -47,7 +35,7 @@ export default function SelfTestPage() {
             setResultState("loading");
             timer = setTimeout(() => {
                 setResultState("nowarn");
-            }, 3000);
+            }, 1000);
         }
         return () => {
             clearTimeout(timer);
@@ -56,34 +44,36 @@ export default function SelfTestPage() {
     return (
         <div className="flex flex-col p-4 w-full gap-2 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))]  from-gray-400 via-gray-400 to-gray-300">
             <h1 className="text-2xl font-bold">Self Test Page</h1>
-            <div className="flex flex-col w-full items-center">
-                <div className="flex flex-row gap-1 px-5 justify-evenly w-full h-14 bg-gradient-to-t from-zinc-500 via-zinc-700 to-zinc-800 items-center rounded-l-2xl" style={{
-                    clipPath: "polygon(80% 0, 100% 50%, 80% 100%, 0 100%, 0 50%, 0 0)"
-                }}>
-                    <div className="flex flex-row justify-evenly h-14 w-1/2 mt-2">
+            <div className="flex flex-col w-full">
+                <div className="flex flex-row gap-1 px-5 justify-between w-10/12 h-14 bg-gradient-to-t from-zinc-200 via-zinc-300 to-zinc-400 items-center rounded-t-2xl">
+                    <div className="flex flex-row gap-6 h-14 mt-2">
                         {Array.from({ length: 4 }).map((_, i) => (
-                            <div key={i} class="w-14 h-14 bg-gray-200" style={{
-                                clipPath: "polygon(69% 20%, 100% 50%, 65% 74%, 0 74%, 25% 50%, 0 20%);"
+                            <div key={i} className={cn("w-12 h-12", questionCompletedCount > i ? "bg-pink-500" : "bg-gray-700")} style={{
+                                clipPath: "polygon(69% 20%, 100% 50%, 65% 74%, 0 74%, 25% 50%, 0 20%)"
                             }}></div>
                         ))}
-                        <span className="text-4xl font-semibold text-white mt-1">
-                            {questionCompletedCount}/4
-                        </span>
                     </div>
+                    <span className="text-2xl font-semibold text-black">
+                        {questionCompletedCount}/4
+                    </span>
                 </div>
-                <div className="flex flex-col gap-1 p-2 w-8/12 items-center flex-wrap mr-40 bg-purple-50 rounded-md">
-                    <video className="w-full" controls autoPlay loop muted>
+                <div className="flex flex-col gap-1 p-2 w-10/12 items-center flex-wrap bg-purple-50 rounded-b-md">
+                    <video className="w-9/12" controls autoPlay loop muted>
                         <source src="/path/to/your/video.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
-                    <div className="flex flex-col flex-1 ">
+                    <div className="flex flex-col flex-1 w-full p-2">
                         <div className="flex flex-col w-full bg-gray-200 p-3 rounded-md gap-5">
                             <div className="text-lg gap-2 flex flex-col">
                                 <span className="">
                                     {questionsArray[questionCompletedCount]}
                                 </span>
-                                <select id="questionSelect" value={responses.current[questionCompletedCount]} className="px-2 py-1 border rounded border-purple-900" onChange={(e)=>{
-
+                                <select id="questionSelect" value={responses[questionCompletedCount]} className="px-2 py-1 border rounded border-purple-900" onChange={(e) => {
+                                    setResponses(prev => {
+                                        const newResponses = [...prev];
+                                        newResponses[questionCompletedCount] = e.target.value;
+                                        return newResponses;
+                                    })
                                 }} >
                                     <option disabled value="">Select an option</option>
                                     <option value="yes">Yes</option>
@@ -91,7 +81,11 @@ export default function SelfTestPage() {
                                 </select>
                             </div>
                             <div className="flex flex-row justify-between">
-                                <Button variant="outline" >
+                                <Button variant="outline" onClick={() => {
+                                    if (questionCompletedCount > 0) {
+                                        setQuestionCompletedCount(prev => prev - 1);
+                                    }
+                                }} >
                                     Previous
                                 </Button>
                                 <span id="errormsg" className="text-lg font-semibold text-red-500"></span>
@@ -102,12 +96,20 @@ export default function SelfTestPage() {
                                     }
                                     else if (questionCompletedCount < 3) {
                                         document.getElementById("errormsg").innerText = "";
-                                        responses.current[questionCompletedCount] = document.getElementById("questionSelect").value;
+                                        setResponses(prev => {
+                                            const newResponses = [...prev];
+                                            newResponses[questionCompletedCount] = document.getElementById("questionSelect").value;
+                                            return newResponses;
+                                        });
                                         setQuestionCompletedCount(prev => prev + 1);
                                     }
                                     else if (questionCompletedCount === 3) {
                                         document.getElementById("errormsg").innerText = "";
-                                        responses.current[questionCompletedCount] = document.getElementById("questionSelect").value;
+                                        setResponses(prev => {
+                                            const newResponses = [...prev];
+                                            newResponses[questionCompletedCount] = document.getElementById("questionSelect").value;
+                                            return newResponses;
+                                        });
                                         handleSubmit();
                                     }
                                 }}>
