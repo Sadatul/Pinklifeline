b23ca0521a02040275d34894776830ae14a8679c
sadatul <sadatulislamsadi@gmail.com>
2024-09-20 19:52:26 +0600

    Added heart beat to backend. backend-config is added thought disabled right now. Right now our websocket are working fine with reconnection but if something fails we will enable backend-config that makes the loadbalancer timeout 2 hour. Uncomment the backend_config and the dependency in backend_service. Apply those and apply ingress.yaml. Your good to go. First delete the old ingress

--- deployment/iaac/kubernetes.tf
+++ deployment/iaac/kubernetes.tf
@@ -82,12 +82,22 @@ resource "kubernetes_manifest" "backend_deployment" {
   ]
 }
 
+# resource "kubernetes_manifest" "backend_config" {
+#   manifest = yamldecode(file("${path.module}/k8s/backend-config.yaml"))
+
+#   depends_on = [
+#     google_container_cluster.pinklifeline_cluster,
+#     kubernetes_manifest.backend_deployment
+#   ]
+# }
+
 resource "kubernetes_manifest" "backend_service" {
   manifest = yamldecode(file("${path.module}/k8s/pinklifeline-app-service.yaml"))
 
   depends_on = [
     google_container_cluster.pinklifeline_cluster,
     kubernetes_manifest.backend_deployment
+    # kubernetes_manifest.backend_config
   ]
 }
 
